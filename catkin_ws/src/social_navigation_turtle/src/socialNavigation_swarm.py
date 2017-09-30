43bee551d0374316a1609faf36f320dc75d4e829
Tincho1994 <mh866@cornell.edu>
2017-09-29 22:40:38 -0400

    Implemented collisiona voidance of swarm

--- catkin_ws/src/social_navigation_turtle/src/socialNavigation_swarm.py
+++ catkin_ws/src/social_navigation_turtle/src/socialNavigation_swarm.py
@@ -19,14 +19,18 @@ class turtleLead():
 		self.vel_publisher = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=5)
 		self.pos_subscriber = rospy.Subscriber('/turtle1/pose',Pose,self.pose_callback)
 		self.rate = rospy.Rate(5)
-		self.waypoints = array([[3,3],[5,10],[10,5]])
+		self.waypoints = array([[2,2],[2,10],[5,10],[10,8],[10,2],[5,5]])
 		self.curWaypt = 0
 		(self.numWaypt,null) =  shape(self.waypoints)
 		self.numWaypt = self.numWaypt -1;
-
+	# --------------------------
+	# CALLBACK FUNCTIONS
+	# -------------------------
 	def pose_callback(self,data):
 		self.pose = data
-
+	# ------------------------------------
+	# MOVEMENT / PATH PLANNING FUNCTIONS
+	# ------------------------------------
 	def sendVel(self,vx,omega):
 		vel_msg = Twist()
 		vel_msg.linear.x = vx
@@ -39,7 +43,7 @@ class turtleLead():
 		self.sendVel(v[0],v[1])
 
 	def calcVel(self,wayPt):
-		#wayPt in form [x,y]
+		# Convert global x,y velocity to turtle fram x velocity and omega
 		pose = self.pose
 		epsilon =0.3
 		xTurt = pose.x; yTurt = pose.y; thetaTurt = pose.theta; 
@@ -61,6 +65,15 @@ class turtleLead():
 		return vOut
 
 class turtleFol():
+	#-------------------
+	# Follower Class
+	#-------------------
+	# This turtle follows the lead turtle. Following is done by having the turtle
+	# move towards the location of the lead turtle, the waypoints the lead is using are
+	# unkown to this turtle.
+	# This turtle also keeps track of the locations of other followers and uses a repulsive force
+	# to modify it's target velocity and avoid collision
+
 	def __init__(self,name,otherFol):
 		rospy.init_node('turtleLead', anonymous=True)
 		self.vel_publisher = rospy.Publisher('/'+name+'/cmd_vel',Twist, queue_size=5)
@@ -76,23 +89,30 @@ class turtleFol():
 		self.leadVel = Twist()
 		self.leadPos = Pose()
 
+	# --------------------------
+	# CALLBACK FUNCTIONS
+	# -------------------------
 	def pose_callback(self,data):
 		self.pose = data
+
 	def velLead_callback(self,data):
 		thetaTurt = self.leadPos.theta
 		data.linear.x = cos(thetaTurt)*data.linear.x
 		data.linear.y = sin(thetaTurt)*data.linear.x
 		self.leadVel  = data
+
 	def posLead_callback(self,data):
 		self.leadPos = data
+
 	def posOther_callback(self,data):
-		# Get message info and topic
 		info = data._connection_header
 		topic = info['topic']
-		# Get name of turtle to which message belongs to
 		turtle = topic.split("/")[1]
 		self.otherPos[turtle] =  data
 
+	# ------------------------------------
+	# MOVEMENT / PATH PLANNING FUNCTIONS
+	# ------------------------------------
 	def sendVel(self,vx,omega):
 		vel_msg = Twist()
 		vel_msg.linear.x = vx
@@ -100,8 +120,9 @@ class turtleFol():
 		self.vel_publisher.publish(vel_msg)
 
 	def moveToLead(self):
+		# Calculates velocity vector to move towards leader
 		pose = self.pose
-		epsilon =0.3
+		epsilon =1
 		xTurt = pose.x; yTurt = pose.y; thetaTurt = pose.theta; 
 		xWp = self.leadPos.x; yWp = self.leadPos.y;
 		xVect = xWp - xTurt
@@ -110,59 +131,73 @@ class turtleFol():
 		mag = sqrt(xVect**2 + yVect**2)
 		if mag < 1:
 			vGlob = array([[0],[0]])
-		elif mag < 2:
-			vGlob = array([[xVect],[yVect]])/mag
+		elif mag < 4:
+			vGlob = array([[xVect],[yVect]])/(mag*1.25)
 		else:
-			vGlob = array([[xVect],[yVect]])*2/mag
-
+			vGlob = array([[xVect],[yVect]])/(mag*0.8)
+		# Call collision avoidance and thens end velocity
 		vGlob = self.avoidCollision(vGlob)
 		cmds = self.convertVel(vGlob)
 		self.sendVel(cmds[0],cmds[1])
 	def avoidCollision(self,vel):
 		x = self.pose.x
 		y = self.pose.y
+		# Loop through other followers and if close enough apply a repulsive force
+		# to the velocity vector to avoid collision
 		for oth in self.otherPos:
 			x2 =  self.otherPos[oth].x
 			y2 = self.otherPos[oth].y
 			xvect = x-x2
 			yvect = y-y2
 			dist =  sqrt(xvect**2 + yvect**2)
-			if dist < 2:
-				vel[0]=vel[0]+xvect
-				vel[1]=vel[1]+yvect
-			return vel
+			if dist < 1:
+				vel[0]=vel[0]+xvect/dist
+				vel[1]=vel[1]+yvect/dist
+		mag = sqrt(vel[0]**2 + vel[1]**2)
+		if (mag>0):
+			vel = vel/mag
+		return vel
 	def convertVel(self,vel):
+		# Convert global x,y velocity to turtle fram x velocity and omega
 		epsilon =1
 		thetaTurt = self.pose.theta
 
 		R = array([[cos(thetaTurt),sin(thetaTurt)],[-sin(thetaTurt),cos(thetaTurt)]])
 		T = dot(array([[1,0],[0,1/epsilon]]),R)
 		vOut = dot(T,vel)
-
+		
 		return vOut
 
-	def copyT(self):
-		vOut = self.convertVel()
-		self.sendVel(vOut[0],vOut[1])
-		
-
-
+# -------------------
+# MAIN FUNCTION
+# -------------------
 if __name__ =='__main__':
+	# Init lead turtle
 	lead = turtleLead()
-	fol = turtleFol('turtle2',['turtle3'])
-	fol2 = turtleFol('turtle3',['turtle2'])
-	vel_msg = Twist()
-	vel_msg.linear.x = 1
+
+	# Create followers
+	# -------------------------------------------------------------------------
+	# SPECIFY NUMBER OF FOLLOWER TURTLES SPAWNED HER, CODE WON'T WORK OTHERWISE
+	# -------------------------------------------------------------------------
+	numFollowers = 16
+	fol = []*numFollowers
+	for i in range(2,numFollowers+2):
+		others = []*numFollowers
+		others.append('turtle1')
+		for j in range(2,numFollowers+2):
+			if (j != i):
+				others.append('turtle'+str(j))
+		fol.append(turtleFol('turtle'+str(i),others))
+	# Start script timer
 	timenow = time.time()
 	startT = time.time()
 	lead.rate.sleep()
-	fol.rate.sleep()
-	while(time.time()-startT < 15):
+
+	# While timer running -> have lead turtle go to waypoints and followers follow
+	while(time.time()-startT < 40):
 		lead.moveTo_wayPt()
-		fol.moveToLead()
-		fol2.moveToLead()
+		for f in fol:
+			f.moveToLead()
 		lead.rate.sleep()
-		fol.rate.sleep()
-		fol2.rate.sleep()
 		pass
 	
--- catkin_ws/src/social_navigation_turtle/src/turtle_swarm_v1.py
+++ catkin_ws/src/social_navigation_turtle/src/turtle_swarm_v1.py
@@ -19,7 +19,7 @@ class turtleLead():
 		self.vel_publisher = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=5)
 		self.pos_subscriber = rospy.Subscriber('/turtle1/pose',Pose,self.pose_callback)
 		self.rate = rospy.Rate(5)
-		self.waypoints = array([[3,3],[5,10],[10,5]])
+		self.waypoints = array([[2,2],[2,10],[5,10],[10,8],[10,2],[5,5]])
 		self.curWaypt = 0
 		(self.numWaypt,null) =  shape(self.waypoints)
 		self.numWaypt = self.numWaypt -1;
@@ -101,7 +101,7 @@ class turtleFol():
 
 	def moveToLead(self):
 		pose = self.pose
-		epsilon =0.3
+		epsilon =1
 		xTurt = pose.x; yTurt = pose.y; thetaTurt = pose.theta; 
 		xWp = self.leadPos.x; yWp = self.leadPos.y;
 		xVect = xWp - xTurt
@@ -110,10 +110,10 @@ class turtleFol():
 		mag = sqrt(xVect**2 + yVect**2)
 		if mag < 1:
 			vGlob = array([[0],[0]])
-		elif mag < 2:
-			vGlob = array([[xVect],[yVect]])/mag
+		elif mag < 4:
+			vGlob = array([[xVect],[yVect]])/(mag*1.25)
 		else:
-			vGlob = array([[xVect],[yVect]])*2/mag
+			vGlob = array([[xVect],[yVect]])/(mag*0.8)
 
 		vGlob = self.avoidCollision(vGlob)
 		cmds = self.convertVel(vGlob)
@@ -127,10 +127,13 @@ class turtleFol():
 			xvect = x-x2
 			yvect = y-y2
 			dist =  sqrt(xvect**2 + yvect**2)
-			if dist < 2:
-				vel[0]=vel[0]+xvect
-				vel[1]=vel[1]+yvect
-			return vel
+			if dist < 1:
+				vel[0]=vel[0]+xvect/dist
+				vel[1]=vel[1]+yvect/dist
+		mag = sqrt(vel[0]**2 + vel[1]**2)
+		if (mag>0):
+			vel = vel/mag
+		return vel
 	def convertVel(self,vel):
 		epsilon =1
 		thetaTurt = self.pose.theta
@@ -138,7 +141,7 @@ class turtleFol():
 		R = array([[cos(thetaTurt),sin(thetaTurt)],[-sin(thetaTurt),cos(thetaTurt)]])
 		T = dot(array([[1,0],[0,1/epsilon]]),R)
 		vOut = dot(T,vel)
-
+		
 		return vOut
 
 	def copyT(self):
@@ -149,20 +152,23 @@ class turtleFol():
 
 if __name__ =='__main__':
 	lead = turtleLead()
-	fol = turtleFol('turtle2',['turtle3'])
-	fol2 = turtleFol('turtle3',['turtle2'])
-	vel_msg = Twist()
-	vel_msg.linear.x = 1
+	numFollowers = 16
+	fol = []*numFollowers
+	for i in range(2,numFollowers+2):
+		others = []*numFollowers
+		others.append('turtle1')
+		for j in range(2,numFollowers+2):
+			if (j != i):
+				others.append('turtle'+str(j))
+		fol.append(turtleFol('turtle'+str(i),others))
+	
 	timenow = time.time()
 	startT = time.time()
 	lead.rate.sleep()
-	fol.rate.sleep()
-	while(time.time()-startT < 15):
+	while(time.time()-startT < 40):
 		lead.moveTo_wayPt()
-		fol.moveToLead()
-		fol2.moveToLead()
+		for f in fol:
+			f.moveToLead()
 		lead.rate.sleep()
-		fol.rate.sleep()
-		fol2.rate.sleep()
 		pass
 	
